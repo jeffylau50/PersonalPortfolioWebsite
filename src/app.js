0a73e080f7841d41bca4c6ee0d2770f84ec6d0df
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-04-18 15:39:57 -0700

    setup

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './navbar.js'
+
+function App() {
+    return(
+        <div>     
+        <Navbar />
+        </div>
+    )
+}
+
+export default App;
