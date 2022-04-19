db67a537448633b51f39e94ad836b4decdef64fe
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-04-18 21:01:38 -0700

    Tech Stack Component

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './navbar.js'
 import Header from './header.js'
+import Techstack from './techstack.js';
 
 function App() {
     return(
         <div>     
         <Navbar />
         <Header />
+        <Techstack />
         </div>
     )
 }
